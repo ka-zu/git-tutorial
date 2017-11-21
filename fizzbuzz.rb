44378dfede55ea00af0301282da39f879eaad252
Kazuma <j1422@matsue-ct.jp>
2017-11-21 09:51:07 +0900

    make fizzbuzz program

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,3 +1,14 @@
 for i in 1..100
-
+	if(i%3 == 0 && i%5 == 0){
+		print("FizzBuzz\n");
+	}
+	elsif(i%3 == 0){
+		print("Fizz\n");
+	}
+	elsif(i%5 == 0){
+		print("buzz\n");
+	}
+	else{
+		print(i, "\n");
+	}
 end
